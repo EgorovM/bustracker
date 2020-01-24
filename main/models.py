23142e21c5ce8be22732c64c1e52e10730af1f09
 Егоров Мичил <michil@MacBook-Pro-Micil.local>
2020-01-24 16:06:50 +0900

    add type and route of bus and fix hidden buses

--- main/models.py
+++ main/models.py
@@ -3,9 +3,11 @@ from django.db import models
 class Bus(models.Model):
     driver_name = models.CharField(max_length = 30)
     bus_number  = models.CharField(max_length = 30)
-
     latitude  = models.FloatField(default = 0)
     longitude = models.FloatField(default = 0)
+
+    type      = models.CharField(default = "city", max_length = 100)
+    route     = models.CharField(default = "Через город", max_length = 100)
 
     def __str__(self):
         return self.driver_name + " " + self.bus_number
